01945ead947618d0c359e3543d7ea032a5694f7b
nevo0309 <nevo19970309@gmail.com.com>
2024-10-26 16:57:01 +0300

    Implemant getRandomInt and update ball1 style

--- js/main.js
+++ js/main.js
@@ -2,13 +2,18 @@
 
 function onBallClick() {
   let elBall1 = document.querySelector('.ball1')
-  let elBall1Size = parseInt(getComputedStyle(elBall1).width)
-  let newWidth = elBall1Size + 50
+  let elBall1Width = parseInt(getComputedStyle(elBall1).width)
+  let elBall1Height = parseInt(getComputedStyle(elBall1).height)
+  let newWidth = elBall1Width + getRandomInt(20, 60)
+  let newHeight = elBall1Height + getRandomInt(20, 60)
+
+  elBall1.style.height = newHeight + 'px'
   elBall1.style.width = newWidth + 'px'
   elBall1.innerHTML = newWidth
 
   if (newWidth > 400) {
     elBall1.style.width = '100px'
+    elBall1.style.height = '100px'
     elBall1.style.transition = '1s'
     elBall1.innerHTML = '100'
   }
