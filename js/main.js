caab3e8f1a0778a5684ae3d40696f917ab7db605
nevo0309 <nevo19970309@gmail.com.com>
2024-10-26 13:11:57 +0300

    Change the function onBallClick to increase the width of ball1

--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
 'use strict'
 
 function onBallClick() {
-  console.log('Ball Clicked')
+  let elBall1 = document.querySelector('.ball1')
+  let elBall1Size = parseInt(getComputedStyle(elBall1).width)
+  elBall1.style.width = elBall1Size + 50 + 'px'
 }
