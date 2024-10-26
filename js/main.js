35465c1b40c3f5c15f8e71f4c34a6c2b896ecb33
nevo0309 <nevo19970309@gmail.com.com>
2024-10-26 13:26:10 +0300

    Add new functionality to the onBallClick

--- js/main.js
+++ js/main.js
@@ -3,5 +3,13 @@
 function onBallClick() {
   let elBall1 = document.querySelector('.ball1')
   let elBall1Size = parseInt(getComputedStyle(elBall1).width)
-  elBall1.style.width = elBall1Size + 50 + 'px'
+  let newWidth = elBall1Size + 50
+  elBall1.style.width = newWidth + 'px'
+  elBall1.innerHTML = newWidth
+
+  if (newWidth > 400) {
+    elBall1.style.width = '100px'
+    elBall1.style.transition = '1s'
+    elBall1.innerHTML = '100'
+  }
 }
